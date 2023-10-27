20d25f3c13ac26bebb6d55343ae9fcd2e692d121
Roukkan <114461369+Roukkan@users.noreply.github.com>
2023-10-27 17:29:51 +0800

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/users.js";
 import { recipesRouter } from "./routes/recipes.js";
+import path from "path";
 
 const app = express();
 
@@ -15,5 +16,14 @@ app.use("/recipes", recipesRouter);
 mongoose.connect(
   "mongodb+srv://kitchenkonnect:kkpass123@recipes.plsmooa.mongodb.net/recipes?retryWrites=true&w=majority"
 );
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Serve the main React app on all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(3001, () => console.log("SERVER STARTED"));
